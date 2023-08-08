a7112310a4f958277d1572812b2d7ffe25761195
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-08-08 07:48:24 +0000

    add optuna distributed example

---
+++ bencher/example/experimental/optuna_dist.py
@@ -0,0 +1,22 @@
+import random
+import time
+
+import optuna
+import optuna_distributed
+from dask.distributed import Client
+
+
+def objective(trial):
+    x = trial.suggest_float("x", -100, 100)
+    y = trial.suggest_categorical("y", [-1, 0, 1])
+    # Some expensive model fit happens here...
+    time.sleep(random.uniform(1.0, 2.0))
+    return x**2 + y
+
+
+if __name__ == "__main__":
+    # client = Client("<your.cluster.scheduler.address>")  # Enables distributed optimization.
+    client = None  # Enables local asynchronous optimization.
+    study = optuna_distributed.from_study(optuna.create_study(), client=client)
+    study.optimize(objective, n_trials=10)
+    print(study.best_value)
