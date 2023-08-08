e978ba9022d614a8dbd0618ab83c30921b683aae
Austin Gregg-Smith <austin.gregg-smith@dyson.com>
2023-08-08 09:38:34 +0000

    add instructions for starting dask

--- bencher/example/experimental/optuna_dist.py
+++ bencher/example/experimental/optuna_dist.py
@@ -10,13 +10,27 @@ def objective(trial):
     x = trial.suggest_float("x", -100, 100)
     y = trial.suggest_categorical("y", [-1, 0, 1])
     # Some expensive model fit happens here...
-    time.sleep(random.uniform(1.0, 2.0))
+    # time.sleep(random.uniform(1.0, 2.0))
     return x**2 + y
 
 
 if __name__ == "__main__":
-    # client = Client("<your.cluster.scheduler.address>")  # Enables distributed optimization.
+    # client = Client("tcp://172.17.0.3:8786")  # Enables distributed optimization.
     client = None  # Enables local asynchronous optimization.
     study = optuna_distributed.from_study(optuna.create_study(), client=client)
-    study.optimize(objective, n_trials=10)
+    study.optimize(objective, n_trials=1000)
     print(study.best_value)
+
+
+# remote
+# docker run -e EXTRA_PIP_PACKAGES="holobench" --network dask ghcr.io/dask/dask dask-worker scheduler:8786 # start worker
+
+# docker run --network dask -p 8787:8787 --name scheduler ghcr.io/dask/dask dask-scheduler  # start scheduler
+
+# docker run -e EXTRA_PIP_PACKAGES="holobench" --network dask ghcr.io/dask/dask dask-worker scheduler:8786 # start worker
+
+
+# local
+# dask scheduler
+# then
+# dask worker tcp://172.19.0.2:8786 --nworkers 15 --nthreads 1
