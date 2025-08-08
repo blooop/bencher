effc9dfce45b03080a6830dfeb67e73e8a5a01f4
Austin Gregg-Smith <blooop@gmail.com>
2025-08-08 12:20:23 +0000

    save as html

--- bencher/bench_runner.py
+++ bencher/bench_runner.py
@@ -193,7 +193,7 @@ class BenchRunner:
             debug (bool): Whether to enable debug mode for publishing
         """
         if save:
-            report.save_index(filename=self.name)
+            report.save_index(filename=f"{self.name}.html")
         if publish and self.publisher is not None:
             if isinstance(self.publisher, GithubPagesCfg):
                 p = self.publisher
