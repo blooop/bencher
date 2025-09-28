833db7b0c680c8aded1d32653cd1f673d0ad3340
Austin Gregg-Smith <blooop@gmail.com>
2025-09-28 11:54:44 +0100

    update doc generation code

--- bencher/example/meta/generate_examples.py
+++ bencher/example/meta/generate_examples.py
@@ -19,15 +19,12 @@ def convert_example_to_jupyter_notebook(
     split_code = example_code.split("""if __name__ == "__main__":""")
     code += split_code[0]
 
-    code += f"""
-bench = {function_name}
-"""
+    code += f"bench = {function_name}"
 
-    code_results = """
-from bokeh.io import output_notebook
+    code_results = """from bokeh.io import output_notebook
+
 output_notebook()
-bench.get_result().to_auto_plots()
-"""
+bench.get_result().to_auto_plots()"""
 
     nb["cells"] = [
         nbf.v4.new_markdown_cell(text),
