67567ef77385a6df4e159054e58560d5ce3bf6f8
Austin Gregg-Smith <blooop@gmail.com>
2025-09-28 12:31:21 +0100

    update pixi.lock

--- docs/conf.py
+++ docs/conf.py
@@ -78,7 +78,7 @@ nbsite_gallery_conf = {
                 {"path": "2D", "title": "2D"},
                 {"path": "levels", "title": "Levels"},
                 {"path": "examples", "title": "Examples"},
-                {"path": "Meta", "title": "Meta"},
+                {"path": "meta", "title": "Meta"},
                 {"path": "yaml", "title": "YAML"},
                 {"path": "media", "title": "Media"},
             ],
